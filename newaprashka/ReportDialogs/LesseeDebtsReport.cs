ed0baf4b06efde6897ac56ab3d8d8f7e8f8af8e5
Gankov Andrey <gav@qsolution.ru>
2014-01-28 17:02:09 +0400

    Возможность детализировать отчет о долгах арендаторов по услугам.

--- newaprashka/ReportDialogs/LesseeDebtsReport.cs
+++ newaprashka/ReportDialogs/LesseeDebtsReport.cs
@@ -1,15 +1,66 @@
 using System;
+using Gtk;
 using QSProjectsLib;
+using MySql.Data.MySqlClient;
 
 namespace bazar
 {
 	public partial class LesseeDebtsReport : Gtk.Dialog
 	{
+		ListStore ServicesList;
+
 		public LesseeDebtsReport ()
 		{
 			this.Build ();
 
 			dateReport.Date = DateTime.Today;
+
+			ServicesList = new ListStore (typeof(int), typeof(bool), typeof(string));
+
+			CellRendererToggle CellSelect = new CellRendererToggle();
+			CellSelect.Activatable = true;
+			CellSelect.Toggled += onCellSelectToggled;
+
+			treeviewServices.AppendColumn ("Выб.", CellSelect, "active", 1);
+			treeviewServices.AppendColumn ("Услуги", new CellRendererText (), "text", 2);
+
+			treeviewServices.Model = ServicesList;
+
+			MainClass.StatusMessage("Запрос типов услуг...");
+			string sql = "SELECT id, name FROM services";
+			try
+			{
+				MySqlCommand cmd = new MySqlCommand(sql, QSMain.connectionDB);
+
+				using(MySqlDataReader rdr = cmd.ExecuteReader())
+				{
+					while (rdr.Read())
+					{
+						ServicesList.AppendValues(rdr.GetInt32 ("id"),
+						                        false,
+						                        rdr.GetString ("name")
+						                       );
+					}
+				}
+				MainClass.StatusMessage("Ok");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+				MainClass.StatusMessage("Ошибка получения информации о услугах!");
+				QSMain.ErrorMessage(this, ex);
+			}
+		}
+
+		void onCellSelectToggled(object o, ToggledArgs args) 
+		{
+			TreeIter iter;
+
+			if (ServicesList.GetIter (out iter, new TreePath(args.Path))) 
+			{
+				bool old = (bool) ServicesList.GetValue(iter,1);
+				ServicesList.SetValue(iter, 1, !old);
+			}
 		}
 
 		protected void OnButtonOkClicked (object sender, EventArgs e)
@@ -19,7 +70,23 @@ namespace bazar
 			string param = "Day=" + dateReport.Date.Day.ToString() +
 				"&Month=" + dateReport.Date.Month.ToString() +
 					"&Year=" + dateReport.Date.Year.ToString();
-			ViewReportExt.Run ("LesseeDebts", param);
+			if(checkDetail.Active)
+			{
+				param += "&Services=";
+				foreach(object[] row in ServicesList)
+				{
+					if ((bool) row [1])
+						param += String.Format ("{0},", row [0]);
+				}
+				ViewReportExt.Run ("LesseeDebtsDetail", param.TrimEnd (','));
+			}
+			else
+				ViewReportExt.Run ("LesseeDebts", param);
+		}
+
+		protected void OnCheckDetailClicked(object sender, EventArgs e)
+		{
+			treeviewServices.Sensitive = checkDetail.Active;
 		}
 	}
 }
--- newaprashka/gtk-gui/bazar.LesseeDebtsReport.cs
+++ newaprashka/gtk-gui/bazar.LesseeDebtsReport.cs
@@ -8,6 +8,9 @@ namespace bazar
 		private global::Gtk.Label label2;
 		private global::QSWidgetLib.DatePicker dateReport;
 		private global::Gtk.Label label1;
+		private global::Gtk.CheckButton checkDetail;
+		private global::Gtk.ScrolledWindow GtkScrolledWindow;
+		private global::Gtk.TreeView treeviewServices;
 		private global::Gtk.Button buttonCancel;
 		private global::Gtk.Button buttonOk;
 
@@ -49,22 +52,48 @@ namespace bazar
 			w1.Add (this.hbox1);
 			global::Gtk.Box.BoxChild w4 = ((global::Gtk.Box.BoxChild)(w1 [this.hbox1]));
 			w4.Position = 0;
+			w4.Expand = false;
 			w4.Fill = false;
 			// Container child dialog1_VBox.Gtk.Box+BoxChild
 			this.label1 = new global::Gtk.Label ();
 			this.label1.Name = "label1";
-			this.label1.LabelProp = global::Mono.Unix.Catalog.GetString ("* Если в договоре не указан день оплаты,\n  долг попадет в отчет с первого числа.");
+			this.label1.LabelProp = global::Mono.Unix.Catalog.GetString ("* Если в договоре не указан день оплаты,\n  долг попадет в отчет с первого числа.\n* Детализированный отчет покажет суммы\n  только по выбранным услугам.");
 			this.label1.Wrap = true;
 			w1.Add (this.label1);
 			global::Gtk.Box.BoxChild w5 = ((global::Gtk.Box.BoxChild)(w1 [this.label1]));
 			w5.Position = 1;
-			w5.Fill = false;
+			w5.Expand = false;
+			// Container child dialog1_VBox.Gtk.Box+BoxChild
+			this.checkDetail = new global::Gtk.CheckButton ();
+			this.checkDetail.CanFocus = true;
+			this.checkDetail.Name = "checkDetail";
+			this.checkDetail.Label = global::Mono.Unix.Catalog.GetString ("Детализировать по услугам");
+			this.checkDetail.DrawIndicator = true;
+			this.checkDetail.UseUnderline = true;
+			w1.Add (this.checkDetail);
+			global::Gtk.Box.BoxChild w6 = ((global::Gtk.Box.BoxChild)(w1 [this.checkDetail]));
+			w6.Position = 2;
+			w6.Expand = false;
+			w6.Fill = false;
+			// Container child dialog1_VBox.Gtk.Box+BoxChild
+			this.GtkScrolledWindow = new global::Gtk.ScrolledWindow ();
+			this.GtkScrolledWindow.Name = "GtkScrolledWindow";
+			this.GtkScrolledWindow.ShadowType = ((global::Gtk.ShadowType)(1));
+			// Container child GtkScrolledWindow.Gtk.Container+ContainerChild
+			this.treeviewServices = new global::Gtk.TreeView ();
+			this.treeviewServices.Sensitive = false;
+			this.treeviewServices.CanFocus = true;
+			this.treeviewServices.Name = "treeviewServices";
+			this.GtkScrolledWindow.Add (this.treeviewServices);
+			w1.Add (this.GtkScrolledWindow);
+			global::Gtk.Box.BoxChild w8 = ((global::Gtk.Box.BoxChild)(w1 [this.GtkScrolledWindow]));
+			w8.Position = 3;
 			// Internal child bazar.LesseeDebtsReport.ActionArea
-			global::Gtk.HButtonBox w6 = this.ActionArea;
-			w6.Name = "dialog1_ActionArea";
-			w6.Spacing = 10;
-			w6.BorderWidth = ((uint)(5));
-			w6.LayoutStyle = ((global::Gtk.ButtonBoxStyle)(4));
+			global::Gtk.HButtonBox w9 = this.ActionArea;
+			w9.Name = "dialog1_ActionArea";
+			w9.Spacing = 10;
+			w9.BorderWidth = ((uint)(5));
+			w9.LayoutStyle = ((global::Gtk.ButtonBoxStyle)(4));
 			// Container child dialog1_ActionArea.Gtk.ButtonBox+ButtonBoxChild
 			this.buttonCancel = new global::Gtk.Button ();
 			this.buttonCancel.CanDefault = true;
@@ -74,9 +103,9 @@ namespace bazar
 			this.buttonCancel.UseUnderline = true;
 			this.buttonCancel.Label = "gtk-cancel";
 			this.AddActionWidget (this.buttonCancel, -6);
-			global::Gtk.ButtonBox.ButtonBoxChild w7 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w6 [this.buttonCancel]));
-			w7.Expand = false;
-			w7.Fill = false;
+			global::Gtk.ButtonBox.ButtonBoxChild w10 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w9 [this.buttonCancel]));
+			w10.Expand = false;
+			w10.Fill = false;
 			// Container child dialog1_ActionArea.Gtk.ButtonBox+ButtonBoxChild
 			this.buttonOk = new global::Gtk.Button ();
 			this.buttonOk.CanDefault = true;
@@ -86,16 +115,17 @@ namespace bazar
 			this.buttonOk.UseUnderline = true;
 			this.buttonOk.Label = "gtk-ok";
 			this.AddActionWidget (this.buttonOk, -5);
-			global::Gtk.ButtonBox.ButtonBoxChild w8 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w6 [this.buttonOk]));
-			w8.Position = 1;
-			w8.Expand = false;
-			w8.Fill = false;
+			global::Gtk.ButtonBox.ButtonBoxChild w11 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w9 [this.buttonOk]));
+			w11.Position = 1;
+			w11.Expand = false;
+			w11.Fill = false;
 			if ((this.Child != null)) {
 				this.Child.ShowAll ();
 			}
-			this.DefaultWidth = 331;
-			this.DefaultHeight = 149;
+			this.DefaultWidth = 374;
+			this.DefaultHeight = 410;
 			this.Show ();
+			this.checkDetail.Clicked += new global::System.EventHandler (this.OnCheckDetailClicked);
 			this.buttonOk.Clicked += new global::System.EventHandler (this.OnButtonOkClicked);
 		}
 	}
