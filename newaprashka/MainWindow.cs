49a3a162e5af6aa3882e3d995313159678efe9e1
Gankov Andrey <gav@qsolution.ru>
2014-03-25 15:26:50 +0400

    Отображение так же номера билда если указан.

--- newaprashka/MainWindow.cs
+++ newaprashka/MainWindow.cs
@@ -412,7 +412,7 @@ public partial class MainWindow : Gtk.Window
 		dialog.ProgramName = "QS: База Арендаторов";
 
 		Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
-		dialog.Version = String.Format("{0}.{1}.{2}", version.Major, version.Minor, version.Build);
+		dialog.Version = version.ToString (version.Build == 0 ? 3 : 4);
 		
 		dialog.Logo = Gdk.Pixbuf.LoadFromResource ("bazar.icons.logo.png");
 		
