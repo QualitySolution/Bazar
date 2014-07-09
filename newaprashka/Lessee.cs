a397d839271ffbba4dcef77a2e43de671582b88f
Gankov Andrey <gav@qsolution.ru>
2014-07-09 18:03:20 +0400

    Не сохранялся комментарий к арендатору.

--- newaprashka/Lessee.cs
+++ newaprashka/Lessee.cs
@@ -137,7 +137,7 @@ namespace bazar
 				cmd.Parameters.AddWithValue("@wholesaler", checkBwholesaler.Active);
 				cmd.Parameters.AddWithValue("@retail", checkBretail.Active);				
 				cmd.Parameters.AddWithValue("@goods_id", DBWorks.ValueOrNull (!GoodsNull, Goods_id));
-				cmd.Parameters.AddWithValue("@comments", DBWorks.ValueOrNull (textviewComments.Buffer.Text == "", textviewComments.Buffer.Text));
+				cmd.Parameters.AddWithValue("@comments", DBWorks.ValueOrNull (textviewComments.Buffer.Text != "", textviewComments.Buffer.Text));
 				
 				cmd.ExecuteNonQuery();
 				MainClass.StatusMessage("Ok");
