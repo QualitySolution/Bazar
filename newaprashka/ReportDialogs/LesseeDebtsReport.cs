e5e3fcf5b030750b1de1be07f9a12304c96fe264
anri-vin <anri.vin@gmail.com>
2015-04-02 09:06:32 +0300

    Добавил проверку на null для ServiceList.

--- newaprashka/ReportDialogs/LesseeDebtsReport.cs
+++ newaprashka/ReportDialogs/LesseeDebtsReport.cs
@@ -7,7 +7,7 @@ namespace bazar
 {
 	public partial class LesseeDebtsReport : Gtk.Dialog
 	{
-		private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+		private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger ();
 		ListStore ServicesList;
 
 		public LesseeDebtsReport ()
@@ -21,7 +21,7 @@ namespace bazar
 
 			ServicesList = new ListStore (typeof(int), typeof(bool), typeof(string));
 
-			CellRendererToggle CellSelect = new CellRendererToggle();
+			CellRendererToggle CellSelect = new CellRendererToggle ();
 			CellSelect.Activatable = true;
 			CellSelect.Toggled += onCellSelectToggled;
 
@@ -30,27 +30,22 @@ namespace bazar
 
 			treeviewServices.Model = ServicesList;
 
-			logger.Info("Запрос типов услуг...");
+			logger.Info ("Запрос типов услуг...");
 			string sql = "SELECT id, name FROM services";
-			try
-			{
-				MySqlCommand cmd = new MySqlCommand(sql, QSMain.connectionDB);
+			try {
+				MySqlCommand cmd = new MySqlCommand (sql, QSMain.connectionDB);
 
-				using(MySqlDataReader rdr = cmd.ExecuteReader())
-				{
-					while (rdr.Read())
-					{
-						ServicesList.AppendValues(rdr.GetInt32 ("id"),
-						                        false,
-						                        rdr.GetString ("name")
-						                       );
+				using (MySqlDataReader rdr = cmd.ExecuteReader ()) {
+					while (rdr.Read ()) {
+						ServicesList.AppendValues (rdr.GetInt32 ("id"),
+						                          false,
+						                          rdr.GetString ("name")
+						);
 					}
 				}
-				logger.Info("Ok");
-			}
-			catch (Exception ex)
-			{
-				QSMain.ErrorMessageWithLog(this, "Ошибка получения информации о услугах!", logger, ex);
+				logger.Info ("Ok");
+			} catch (Exception ex) {
+				QSMain.ErrorMessageWithLog (this, "Ошибка получения информации о услугах!", logger, ex);
 			}
 
 			comboCash.Active = 0;
@@ -60,60 +55,55 @@ namespace bazar
 		protected	void TestCanSave ()
 		{
 			bool Servicesok = !checkDetail.Active;
-			foreach(object[] row in ServicesList)
-			{
-				if ((bool) row [1])
-				{
-					Servicesok = true;
-					break;
+			if (ServicesList != null) {
+				foreach (object[] row in ServicesList) {
+					if ((bool)row [1]) {
+						Servicesok = true;
+						break;
+					}
 				}
 			}
-
 			buttonOk.Sensitive = Servicesok;
 		}
 
-		void onCellSelectToggled(object o, ToggledArgs args) 
+		void onCellSelectToggled (object o, ToggledArgs args)
 		{
 			TreeIter iter;
 
-			if (ServicesList.GetIter (out iter, new TreePath(args.Path))) 
-			{
-				bool old = (bool) ServicesList.GetValue(iter,1);
-				ServicesList.SetValue(iter, 1, !old);
+			if (ServicesList.GetIter (out iter, new TreePath (args.Path))) {
+				bool old = (bool)ServicesList.GetValue (iter, 1);
+				ServicesList.SetValue (iter, 1, !old);
 				TestCanSave ();
 			}
 		}
 
 		protected void OnButtonOkClicked (object sender, EventArgs e)
 		{
-			if(dateReport.IsEmpty)
+			if (dateReport.IsEmpty)
 				return;
-			string param = "Day=" + dateReport.Date.Day.ToString() +
-				"&Month=" + dateReport.Date.Month.ToString() +
-				"&Year=" + dateReport.Date.Year.ToString() + 
-				"&Cash=" + ComboWorks.GetActiveId(comboCash) +
-				"&Org=" + ComboWorks.GetActiveId(comboOrg);
-			if(checkDetail.Active)
-			{
+			string param = "Day=" + dateReport.Date.Day.ToString () +
+			               "&Month=" + dateReport.Date.Month.ToString () +
+			               "&Year=" + dateReport.Date.Year.ToString () +
+			               "&Cash=" + ComboWorks.GetActiveId (comboCash) +
+			               "&Org=" + ComboWorks.GetActiveId (comboOrg);
+			if (checkDetail.Active) {
 				param += "&Services=";
-				foreach(object[] row in ServicesList)
-				{
-					if ((bool) row [1])
+				foreach (object[] row in ServicesList) {
+					if ((bool)row [1])
 						param += String.Format ("{0},", row [0]);
 				}
 				ViewReportExt.Run ("LesseeDebtsDetail", param.TrimEnd (','));
-			}
-			else
+			} else
 				ViewReportExt.Run ("LesseeDebts", param);
 		}
 
-		protected void OnCheckDetailClicked(object sender, EventArgs e)
+		protected void OnCheckDetailClicked (object sender, EventArgs e)
 		{
 			treeviewServices.Sensitive = checkDetail.Active;
 			TestCanSave ();
 		}
 
-		protected void OnComboCashChanged(object sender, EventArgs e)
+		protected void OnComboCashChanged (object sender, EventArgs e)
 		{
 			TestCanSave ();
 		}
