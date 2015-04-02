950da6c0568efc4deb4fcdf9cf08f01ea4670110
anri-vin <anri.vin@gmail.com>
2015-04-02 09:31:34 +0300

    Исправление вылета. Очередная проверка на null.

--- newaprashka/CashBalance.cs
+++ newaprashka/CashBalance.cs
@@ -9,115 +9,112 @@ namespace bazar
 {
 	public partial class CashBalance : Gtk.Dialog
 	{
-		private static Logger logger = LogManager.GetCurrentClassLogger();
+		private static Logger logger = LogManager.GetCurrentClassLogger ();
 		TreeStore BalanceTreeStore;
 
 		public CashBalance ()
 		{
 			this.Build ();
 
-			ComboWorks.ComboFillReference(comboCash,"cash", ComboWorks.ListMode.WithAll);
-			ComboWorks.ComboFillReference(comboOrg, "organizations", ComboWorks.ListMode.WithAll);
+			ComboWorks.ComboFillReference (comboCash, "cash", ComboWorks.ListMode.WithAll);
+			ComboWorks.ComboFillReference (comboOrg, "organizations", ComboWorks.ListMode.WithAll);
 
-			BalanceTreeStore = new Gtk.TreeStore (typeof (string), typeof (string));
+			BalanceTreeStore = new Gtk.TreeStore (typeof(string), typeof(string));
 			
 			treeviewBalance.AppendColumn ("Статья", new Gtk.CellRendererText (), "text", 0);
 			treeviewBalance.AppendColumn ("Сумма", new Gtk.CellRendererText (), "text", 1);
 			
 			treeviewBalance.Model = BalanceTreeStore;
-			treeviewBalance.ShowAll();
+			treeviewBalance.ShowAll ();
 			radioMonth.Click ();
 		}
 
-		void UpdateBalance()
+		void UpdateBalance ()
 		{
 			TreeIter iter;
+			if (BalanceTreeStore == null)
+				return;
 			BalanceTreeStore.Clear ();
 			decimal TotalExpense = 0m;
 			decimal TotalIncome = 0m;
-			try
-			{
+			try {
 				// Расходы
 				string sqlwhere = "";
 				string sqlwhere2 = "";
-				if(comboCash.GetActiveIter(out iter) && comboCash.Active != 0)
-				{
-					sqlwhere += " AND cash_id = '" + comboCash.Model.GetValue(iter,1) + "'";
-					sqlwhere2 += " AND credit_slips.cash_id = '" + comboCash.Model.GetValue(iter,1) + "'";
+				if (comboCash.GetActiveIter (out iter) && comboCash.Active != 0) {
+					sqlwhere += " AND cash_id = '" + comboCash.Model.GetValue (iter, 1) + "'";
+					sqlwhere2 += " AND credit_slips.cash_id = '" + comboCash.Model.GetValue (iter, 1) + "'";
 				}
-				if(comboOrg.GetActiveIter(out iter) && comboOrg.Active != 0)
-				{
-					sqlwhere += " AND org_id = '" + comboOrg.Model.GetValue(iter,1) + "'";
-					sqlwhere2 += " AND credit_slips.org_id = '" + comboOrg.Model.GetValue(iter,1) + "'";
+				if (comboOrg.GetActiveIter (out iter) && comboOrg.Active != 0) {
+					sqlwhere += " AND org_id = '" + comboOrg.Model.GetValue (iter, 1) + "'";
+					sqlwhere2 += " AND credit_slips.org_id = '" + comboOrg.Model.GetValue (iter, 1) + "'";
 				}
 				string sql = "SELECT expense_items.name as item, SUM(amount) as sum FROM ( " +
-					"SELECT expense_id, SUM(sum) as amount FROM debit_slips " +
-					"WHERE operation != 'advance' AND date BETWEEN @start AND @end "+ sqlwhere + " " +
-					"GROUP BY expense_id " +
-					"UNION ALL SELECT expense_id, SUM(sum) as amount FROM advance " +
-					"WHERE date BETWEEN @start AND @end "+ sqlwhere + " " +
-					"GROUP BY expense_id ) as result " +
-					"LEFT JOIN expense_items ON result.expense_id = expense_items.id GROUP BY expense_id ";
+				             "SELECT expense_id, SUM(sum) as amount FROM debit_slips " +
+				             "WHERE operation != 'advance' AND date BETWEEN @start AND @end " + sqlwhere + " " +
+				             "GROUP BY expense_id " +
+				             "UNION ALL SELECT expense_id, SUM(sum) as amount FROM advance " +
+				             "WHERE date BETWEEN @start AND @end " + sqlwhere + " " +
+				             "GROUP BY expense_id ) as result " +
+				             "LEFT JOIN expense_items ON result.expense_id = expense_items.id GROUP BY expense_id ";
 
-				MySqlCommand cmd = new MySqlCommand(sql, QSMain.connectionDB);
-				cmd.Parameters.AddWithValue("@start", dateCashStart.Date);
-				cmd.Parameters.AddWithValue("@end", dateCashEnd.Date.AddDays (1));
-				MySqlDataReader rdr = cmd.ExecuteReader();
+				MySqlCommand cmd = new MySqlCommand (sql, QSMain.connectionDB);
+				cmd.Parameters.AddWithValue ("@start", dateCashStart.Date);
+				cmd.Parameters.AddWithValue ("@end", dateCashEnd.Date.AddDays (1));
+				MySqlDataReader rdr = cmd.ExecuteReader ();
 
 				TreeIter FirstLevelIter;
 
-				FirstLevelIter = BalanceTreeStore.AppendValues("Статьи расходов");
+				FirstLevelIter = BalanceTreeStore.AppendValues ("Статьи расходов");
 
-				while (rdr.Read())
-				{
-					TotalExpense += rdr.GetDecimal("sum");
-					BalanceTreeStore.AppendValues(FirstLevelIter,
-						                          rdr["item"].ToString (),
-						                          string.Format("{0:C}",rdr.GetDecimal("sum")));
+				while (rdr.Read ()) {
+					TotalExpense += rdr.GetDecimal ("sum");
+					BalanceTreeStore.AppendValues (FirstLevelIter,
+					                               rdr ["item"].ToString (),
+					                               string.Format ("{0:C}", rdr.GetDecimal ("sum")));
 				}
-				BalanceTreeStore.AppendValues("Итого:",
-				                              string.Format("{0:C}",TotalExpense));
-				rdr.Close();
-				BalanceTreeStore.AppendNode();
+				BalanceTreeStore.AppendValues ("Итого:",
+				                               string.Format ("{0:C}", TotalExpense));
+				rdr.Close ();
+				BalanceTreeStore.AppendNode ();
 
 				// Доходы
 				sql = "SELECT income_items.name as item, SUM(sumtable.total) as sum FROM (" +
-					"SELECT income_id, SUM(sum) as total FROM credit_slips " +
-					"WHERE operation = 'common' AND date BETWEEN @start AND @end "+ sqlwhere + " " +
-					"GROUP BY income_id " +
-					"UNION ALL " +
-					"SELECT payment_details.income_id as income_id, SUM(payment_details.sum) as total " +
-					"FROM payment_details, payments, credit_slips " +
-					"WHERE payment_details.payment_id = payments.id AND payments.credit_slip_id = credit_slips.id " +
-					"AND credit_slips.operation = 'payment' AND credit_slips.date BETWEEN @start AND @end "+ sqlwhere2 + " " +
-					"GROUP BY payment_details.income_id ) as sumtable " +
-					"LEFT JOIN income_items ON income_id = income_items.id " +
-					"GROUP BY sumtable.income_id";
+				"SELECT income_id, SUM(sum) as total FROM credit_slips " +
+				"WHERE operation = 'common' AND date BETWEEN @start AND @end " + sqlwhere + " " +
+				"GROUP BY income_id " +
+				"UNION ALL " +
+				"SELECT payment_details.income_id as income_id, SUM(payment_details.sum) as total " +
+				"FROM payment_details, payments, credit_slips " +
+				"WHERE payment_details.payment_id = payments.id AND payments.credit_slip_id = credit_slips.id " +
+				"AND credit_slips.operation = 'payment' AND credit_slips.date BETWEEN @start AND @end " + sqlwhere2 + " " +
+				"GROUP BY payment_details.income_id ) as sumtable " +
+				"LEFT JOIN income_items ON income_id = income_items.id " +
+				"GROUP BY sumtable.income_id";
 				
-				cmd = new MySqlCommand(sql, QSMain.connectionDB);
-				cmd.Parameters.AddWithValue("@start", dateCashStart.Date);
-				cmd.Parameters.AddWithValue("@end", dateCashEnd.Date.AddDays (1));
-				rdr = cmd.ExecuteReader();
+				cmd = new MySqlCommand (sql, QSMain.connectionDB);
+				cmd.Parameters.AddWithValue ("@start", dateCashStart.Date);
+				cmd.Parameters.AddWithValue ("@end", dateCashEnd.Date.AddDays (1));
+				rdr = cmd.ExecuteReader ();
 				
-				FirstLevelIter = BalanceTreeStore.AppendValues("Статьи Доходов");
+				FirstLevelIter = BalanceTreeStore.AppendValues ("Статьи Доходов");
 				
-				while (rdr.Read())
-				{
-					TotalIncome += rdr.GetDecimal("sum");
-					BalanceTreeStore.AppendValues(FirstLevelIter,
-					                              rdr["item"].ToString (),
-					                              string.Format("{0:C}",rdr.GetDecimal("sum")));
+				while (rdr.Read ()) {
+					TotalIncome += rdr.GetDecimal ("sum");
+					BalanceTreeStore.AppendValues (FirstLevelIter,
+					                               rdr ["item"].ToString (),
+					                               string.Format ("{0:C}", rdr.GetDecimal ("sum")));
 				}
-				BalanceTreeStore.AppendValues("Итого:",
-				                              string.Format("{0:C}",TotalIncome));
-				rdr.Close();
+				BalanceTreeStore.AppendValues ("Итого:",
+				                               string.Format ("{0:C}", TotalIncome));
+				rdr.Close ();
 
-				treeviewBalance.ExpandAll();
+				treeviewBalance.ExpandAll ();
+				
+			} catch (Exception ex) {
+				QSMain.ErrorMessageWithLog (this, "Не удалось сформировать отчет!", logger, ex);
 			}
-			catch (Exception ex)
-			{
-				QSMain.ErrorMessageWithLog(this, "Не удалось сформировать отчет!", logger, ex);
-			}
+
 		}
 
 		protected void OnComboOrgChanged (object sender, EventArgs e)
@@ -142,25 +139,25 @@ namespace bazar
 
 		protected void OnRadioMonthClicked (object sender, EventArgs e)
 		{
-			dateCashStart.Date = DateTime.Now.AddMonths(-1);
+			dateCashStart.Date = DateTime.Now.AddMonths (-1);
 			dateCashEnd.Date = DateTime.Now.Date;
 		}
-		
+
 		protected void OnRadio3monthClicked (object sender, EventArgs e)
 		{
-			dateCashStart.Date = DateTime.Now.AddMonths(-3);
+			dateCashStart.Date = DateTime.Now.AddMonths (-3);
 			dateCashEnd.Date = DateTime.Now.Date;
 		}
-		
+
 		protected void OnRadioYearClicked (object sender, EventArgs e)
 		{
-			dateCashStart.Date = DateTime.Now.AddYears(-1);
+			dateCashStart.Date = DateTime.Now.AddYears (-1);
 			dateCashEnd.Date = DateTime.Now.Date;
 		}
-		
+
 		protected void OnRadioAllClicked (object sender, EventArgs e)
 		{
-			dateCashStart.Date = DateTime.Parse("1.1.2012");
+			dateCashStart.Date = DateTime.Parse ("1.1.2012");
 			dateCashEnd.Date = DateTime.Now.Date;
 		}
 	}
