65177b7dc0fdd6c25fe65ba0659bb8cdd58bdf1a
Andrey Gankov <gav@qsolution.ru>
2015-06-17 16:30:41 +0300

    Добавлена редакция в атрибутах(from salova)

--- newaprashka/AssemblyInfo.cs
+++ newaprashka/AssemblyInfo.cs
@@ -6,6 +6,7 @@ using QSProjectsLib;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("QS: База арендаторов")]
+[assembly: AssemblyEdition(Edition = "gpl")]
 [assembly: AssemblyDescription("Программа позволяет вести учет арендаторов, кассы и т.п.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Quality Solution")]
