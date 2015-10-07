49e804e920e3efb94d50659b03703fefdc42b22e
Andrey Gankov <gav@qsolution.ru>
2015-10-07 17:20:19 +0300

    Подготовка 2.3

--- newaprashka/AssemblyInfo.cs
+++ newaprashka/AssemblyInfo.cs
@@ -20,13 +20,14 @@ using QSProjectsLib;
 
 [assembly: AssemblyAuthor("Ганьков Андрей <gav@qsolution.ru>")]
 [assembly: AssemblyAuthor("Виноградов Андрей <vad@qsolution.ru>")]
+[assembly: AssemblyAuthor("Никитин Дмитрий <nds@qsolution.ru>")]
 
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("2.2.6.4")]
+[assembly: AssemblyVersion("2.3")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
