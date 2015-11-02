a29b4dd62639d52c66a98fe8d6f7cd220a056f29
Dmitry Nikitin <baragon92@gmail.com>
2015-11-02 13:03:21 +0300

    В отчете по поставщикам оплата в следующем месяце вместо текущего

--- newaprashka/ReportDialogs/ServiceProvidersReport.cs
+++ newaprashka/ReportDialogs/ServiceProvidersReport.cs
@@ -14,7 +14,17 @@ namespace bazar
 
 		protected void OnButtonOkClicked (object sender, EventArgs e)
 		{
+			int month = comboMonth.Active+1;
+			int year = Convert.ToInt32(comboYear.ActiveText);
+
 			string args = "month=" + (comboMonth.Active+1).ToString()+"&year="+comboYear.ActiveText;
+			int paymentMonth = month + 1;
+			int paymentYear = year;
+			if (paymentMonth > 12) {
+				paymentMonth %= 12;
+				paymentYear= year+1;
+			}
+			args += "&paymentMonth=" + paymentMonth + "&paymentYear=" + paymentYear;
 			ViewReportExt.Run("ServiceProviderReport", args);
 		}
 	}
