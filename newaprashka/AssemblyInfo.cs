e6515d006fc6b1ece09c6bb6a02cd9611b9e8f9d
Andrey Gankov <gav@qsolution.ru>
2016-04-11 09:01:50 +0300

    Релиз 2.3.0.11

--- newaprashka/AssemblyInfo.cs
+++ newaprashka/AssemblyInfo.cs
@@ -12,8 +12,6 @@ using QSProjectsLib;
 [assembly: AssemblyCompany("Quality Solution")]
 [assembly: AssemblyProduct("QS: База арендаторов")]
 [assembly: AssemblyCopyright("Quality Solution 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: AssemblyLogoIcon("bazar.icons.logo.png")]
 [assembly: AssemblySupport("", true, "MySQL")]
 [assembly: AssemblyAppWebsite("http://bazar.qsolution.ru/")]
@@ -27,7 +25,7 @@ using QSProjectsLib;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("2.3.0.10")]
+[assembly: AssemblyVersion("2.3.0.11")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
