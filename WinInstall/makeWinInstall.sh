94c114291fa4991a0fc1c4c24fef1cb46ab9d4a3
Andrey Gankov <gav@qsolution.ru>
2019-09-13 20:28:17 +0300

    Скрипт доработан под сборку на дженкинсе.

--- WinInstall/makeWinInstall.sh
+++ WinInstall/makeWinInstall.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+cd "$(dirname "$0")"
+
 ProjectName="bazar"
 BinDir=../newaprashka/bin/Release
 
@@ -12,7 +14,12 @@ rm -v -f ${BinDir}/*.mdb
 rm -v -f ${BinDir}/*.pdb
 rm -v -f -R ./Files/*
 
+mkdir -p Files
 cp -r -v ExtraFiles/* ./Files
 cp -r -v ${BinDir}/* ./Files
+
+if [ ! -f "gtk-sharp-2.12.21.msi" ]; then
+    wget https://xamarin.azureedge.net/GTKforWindows/Windows/gtk-sharp-2.12.21.msi
+fi
 
 wine ~/.wine/drive_c/Program\ Files\ \(x86\)/NSIS/makensis.exe /INPUTCHARSET UTF8 ${ProjectName}.nsi
