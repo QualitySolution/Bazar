ce562831134848da9da86fc9aa73547d4e7691aa
Andrey Gankov <gav@qsolution.ru>
2024-03-15 09:48:45 +0300

    Проапдейтил версию.

--- newaprashka/AssemblyInfo.cs
+++ newaprashka/AssemblyInfo.cs
@@ -10,21 +10,21 @@ using QSProjectsLib;
 [assembly: AssemblyDescription("Программа позволяет вести учет арендаторов, кассы и т.п.")]
 [assembly: AssemblyCompany("Quality Solution")]
 [assembly: AssemblyProduct("QS: База арендаторов")]
-[assembly: AssemblyCopyright("Quality Solution 2021")]
+[assembly: AssemblyCopyright("Quality Solution 2024")]
 [assembly: AssemblyLogoIcon("bazar.icons.logo.png")]
 [assembly: AssemblySupport("", true, "MySQL")]
 [assembly: AssemblyAppWebsite("http://bazar.qsolution.ru/")]
 
 [assembly: AssemblyAuthor("Ганьков Андрей <gav@qsolution.ru>")]
-[assembly: AssemblyAuthor("Виноградов Андрей <vad@qsolution.ru>")]
-[assembly: AssemblyAuthor("Никитин Дмитрий <nds@qsolution.ru>")]
+[assembly: AssemblyAuthor("Виноградов Андрей 2015")]
+[assembly: AssemblyAuthor("Никитин Дмитрий 2016")]
 
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("2.3.19")]
+[assembly: AssemblyVersion("2.3.20")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
