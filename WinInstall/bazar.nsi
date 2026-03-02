5553fc321a857d6754e8fd2a308c9bfff7ee92ab
Andrey Gankov <gav@qsolution.ru>
2026-03-02 16:37:07 +0300

    2.3.21

--- WinInstall/bazar.nsi
+++ WinInstall/bazar.nsi
@@ -1,6 +1,6 @@
 Unicode true
 ;--------------------------------
-!define PRODUCT_VERSION "2.3.20"
+!define PRODUCT_VERSION "2.3.21"
 !define NET_VERSION "4.6.1"
 !define PRODUCT_NAME "QS: База арендаторов"
 !define SHORTCUT_NAME "QS База арендаторов"
--- newaprashka/AssemblyInfo.cs
+++ newaprashka/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using QS.Project.Versioning;
 [assembly: AssemblyDescription("Программа позволяет вести учет арендаторов, кассы и т.п.")]
 [assembly: AssemblyCompany("Quality Solution")]
 [assembly: AssemblyProduct("QS: База арендаторов")]
-[assembly: AssemblyCopyright("Quality Solution 2024")]
+[assembly: AssemblyCopyright("Quality Solution 2026")]
 [assembly: AssemblyLogoIcon("bazar.icons.logo.png")]
 [assembly: AssemblySupport("Телефон тех. поддержки +7(812)309-71-77")]
 
@@ -23,7 +23,7 @@ using QS.Project.Versioning;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("2.3.20")]
+[assembly: AssemblyVersion("2.3.21")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
